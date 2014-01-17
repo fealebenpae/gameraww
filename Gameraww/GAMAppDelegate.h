e4b1c668f28ae0a60ed4ab36e9783983fe51d91e
Yavor Georgiev <fealebenpae@gmail.com>
2014-01-17 13:27:38 +0200

    Initial Commit

---
+++ Gameraww/GAMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GAMAppDelegate.h
+//  Gameraww
+//
+//  Created by Yavor Georgiev on 17.01.14.
+//  Copyright (c) 2014 г. Telerik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GAMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gameraww/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Gameraww
+//
+//  Created by Yavor Georgiev on 17.01.14.
+//  Copyright (c) 2014 г. Telerik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GAMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GAMAppDelegate class]));
+    }
+}
